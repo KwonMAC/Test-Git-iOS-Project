b62fee163973edc8ca31d9cd4cf70b14266672b4
Kwon <kwontaehun@Kwonui-Mac-mini.local>
2014-06-08 10:11:03 +0900

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Kwon on 2014. 6. 8..
+//  Copyright (c) 2014년 Code Coalition. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Kwon on 2014. 6. 8..
+//  Copyright (c) 2014년 Code Coalition. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
